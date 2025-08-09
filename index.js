221b219f7bb3e5e82e696943411c6b3459dd696e
Akash <divateakash21@gmail.com>
2025-08-09 16:32:02 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Docker from the container!");
-});
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+// Define PORT first
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker the container!");
+});
+
+// Listen on all network interfaces
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
