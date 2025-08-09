29aa5a60ddbc1b992ec9ea2fd5f33ac0b652cf27
Akash <divateakash21@gmail.com>
2025-08-09 16:20:43 +0530

    Add GitHub Actions for Docker push

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker container!");
+  res.send("Hello from Docker from the container!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 const PORT = process.env.PORT || 3000;
