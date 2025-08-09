1e9c7f347763d3d239aa77265f7a7bd0de22e6cd
Akash <divateakash21@gmail.com>
2025-08-09 16:50:10 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const app = express();
-
-// Define PORT first
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Docker the container!");
-});
+app.get("/", (req, res) => res.send("Hello from Docker!"));
 
-// Listen on all network interfaces
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
